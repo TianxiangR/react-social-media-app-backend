d1fc140c8f51419b782a6ec9e4ce21f0a04e792e
TianxiangR <ren644120622@gmail.com>
2024-02-12 17:44:08 -0700

    fix: update allowed_hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = 'django-insecure-0!50nqsd!h2n1dbygv7z*lap*y_2odlohshrdcfht_7h^)w!sv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ec2-3-16-187-83.us-east-2.compute.amazonaws.com', '3.16.187.83']
+ALLOWED_HOSTS = ['ec2-3-16-187-83.us-east-2.compute.amazonaws.com', '3.16.187.83', 'localhost', 'tianxiang-x.com', 'django-server-1667649642.us-east-2.elb.amazonaws.com']
 
 CORS_ALL = True
 CORS_ALLOW_ALL_ORIGINS = True
